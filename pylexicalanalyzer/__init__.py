881f6edcd021fd91049f18db898c35ec48ddb498
giusniyyel <giusniyyel@gmail.com>
2021-12-25 01:00:23 -0600

    :sparkles: Constants & Ply added

--- pylexicalanalyzer/__init__.py
+++ pylexicalanalyzer/__init__.py
@@ -1,2 +1,9 @@
+# Libraries
+from .ply import lex
+import re
+import codecs
+import os
+import sys
+
 from .helpers import *
 import constants as c
---
+++ pylexicalanalyzer/constants.py
@@ -0,0 +1,37 @@
+tokens = [
+    'ID',
+    'NUMBER',
+    'PLUS',
+    'MINUS',
+    'TIMES',
+    'DIVIDE',
+    'ODD',
+    'ASSIGN',
+    'NE',
+    'LT',
+    'LTE',
+    'GT',
+    'GTE',
+    'LPARENT',
+    'RPARENT',
+    'COMMA',
+    'SEMMICOLOM',
+    'DOT',
+    'UPDATE'
+]
+
+reserved = {
+    'begin':'BEGIN',
+    'end':'END',
+    'if':'IF',
+    'then':'THEN',
+    'while':'WHILE',
+    'do':'DO',
+    'call':'CALL',
+    'const':'CONST',
+    'int':'INT',
+    'procedure':'PROCEDURE',
+    'out':'OUT',
+    'in':'IN',
+    'else':'ELSE'
+}
