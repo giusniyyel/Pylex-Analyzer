384a2c926e30dd0c5b55ff7d496c70d02c438d3c
giusniyyel <giusniyyel@gmail.com>
2022-01-07 12:05:16 -0600

    :sparkles: Add token group handling & printing just the token group number

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 from tokrules import MyLexer
+from helpers import token_group
+
+# Saving tokens values
+data_tokens = []
 
 # Opening source code file
 f = open('test/examples/helloworld.cpp','r')
@@ -17,7 +21,12 @@ while True:
     tok = lexer.token()
     if not tok:
         break      # No more input
-    print(tok)
+    tok_group = token_group(tok.type)
+    data_tokens.append(tok_group)
+    print('[',tok_group,', ',tok,']')
+
+# Print all the tokens
+print('TOKEN: ',data_tokens)
 
 # Closing the source code file
 f.close()
